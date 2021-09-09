fa7dd23c96f0c661635083807e82c39eab814dff
Dipti-S1 <dipti.sunal91@gmail.com>
2021-09-09 06:42:32 +0530

    Fivth version of Medical App

---
+++ springboot-firstwebapp/src/test/java/Test/LoginTest.java
@@ -0,0 +1,35 @@
+package Test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.AssertJUnit;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginTest {
+
+	public static void main(String args[])
+	{
+	  
+	   System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+	   
+	    WebDriver driver = new ChromeDriver();
+	
+        driver.get("https://localhost:8090/login");
+        
+        driver.manage().window().maximize();
+        //WebElement loginlink = driver.findElement(By.linkText("Log in"));
+  
+        
+		WebElement userNameField = driver.findElement(By.name("usernameFieldL"));
+        WebElement passwordField = driver.findElement(By.name("passwordFieldL"));
+        WebElement submitButton = driver.findElement(By.name("submitButtonL"));
+
+        userNameField.sendKeys("standard_user");
+        passwordField.sendKeys("ath4563");
+        submitButton.click();
+        
+	    }
+	}
